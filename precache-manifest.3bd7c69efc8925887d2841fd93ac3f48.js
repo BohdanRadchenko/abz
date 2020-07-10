0cf1cf88aecd76fad5820df61f6bd2947b7fbc73
BohdanRadchenko <radchenkobs@gmail.com>
2020-07-10 17:20:50 +0300

    Updates

---
+++ precache-manifest.3bd7c69efc8925887d2841fd93ac3f48.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3598d61cd66a091ec6fff3cfce67f06f",
+    "url": "/abz/index.html"
+  },
+  {
+    "revision": "87fa7258c5ccaffed26b",
+    "url": "/abz/static/css/4.a09b53da.chunk.css"
+  },
+  {
+    "revision": "7b567eee523632cd19bc",
+    "url": "/abz/static/css/main.a9952a65.chunk.css"
+  },
+  {
+    "revision": "ee8c84ce9e4a5fe1ec5b",
+    "url": "/abz/static/js/2.685ff251.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/abz/static/js/2.685ff251.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "295ce42a55dc8007e650",
+    "url": "/abz/static/js/3.a4dc8fe3.chunk.js"
+  },
+  {
+    "revision": "87fa7258c5ccaffed26b",
+    "url": "/abz/static/js/4.f1248298.chunk.js"
+  },
+  {
+    "revision": "7b567eee523632cd19bc",
+    "url": "/abz/static/js/main.1e4985b7.chunk.js"
+  },
+  {
+    "revision": "00bc0ca2d67ec8c97129",
+    "url": "/abz/static/js/runtime-main.aeab2319.js"
+  },
+  {
+    "revision": "f5443d74025f2a6069e0ca0142c08a05",
+    "url": "/abz/static/media/banner-photo.f5443d74.jpg"
+  },
+  {
+    "revision": "146e29265bcc680129e263a9478dca00",
+    "url": "/abz/static/media/logo.146e2926.svg"
+  },
+  {
+    "revision": "3cdf78971c869b16b95152be7b6922e5",
+    "url": "/abz/static/media/man-laptop-v1.3cdf7897.svg"
+  }
+]);
